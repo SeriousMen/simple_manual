562370eb361ccf06e26c4dc3128f80f184d058be
hhwork <hhwork@naver.com>
2023-09-14 22:12:16 +0900

    react-redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const weight = 200 ;
+function reducer(state = weight, action) {
+  return state 
+} 
+
+let store = createStore(reducer);
+
+
+const root = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
+    <Provider store ={store}>
     <App />
+    </Provider>
   </React.StrictMode>
+, root
 );
 
 // If you want to start measuring performance in your app, pass a function
